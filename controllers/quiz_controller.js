263342c5bbad3c1d9d46e93d1d71026dc14fc15e
Jose Antonio López <joseantoniolopezanguita@gmail.com>
2015-07-14 18:05:08 +0200

    Postgre en heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,19 +1,36 @@
 var models = require('../models/models.js');
 
-// GET /quizes/question
-exports.question = function(req, res) {
-		models.Quiz.findAll().success(function(quiz){
-			res.render('quizes/question', {pregunta: quiz[0].pregunta});
-	})
+// Autoload - factoriza el código si la ruta incluye :quizId
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.find(quizId).then(
+    function(quiz) {
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      } else { next(new Error('No existe quizId=' + quizId)); }
+    }
+  ).catch(function(error) { next(error);});
 };
 
-// GET /quizes/answer
+// GET /quizes
+exports.index = function(req, res) {
+  models.Quiz.findAll().then(
+    function(quizes) {
+      res.render('quizes/index', { quizes: quizes});
+    }
+  ).catch(function(error) { next(error);})
+};
+
+// GET /quizes/:id
+exports.show = function(req, res) {
+  res.render('quizes/show', { quiz: req.quiz});
+};
+
+// GET /quizes/:id/answer
 exports.answer = function(req, res) {
-	models.Quiz.findAll().success(function(quiz){
-		if (req.query.respuesta.toLowerCase() === quiz[0].respuesta.toLowerCase()){
-			res.render('quizes/answer', { respuesta: 'Correcto' });
-		} else {
-			res.render('quizes/answer', { respuesta: 'Incorrecto'});
-		}
-	})
+  var resultado = 'Incorrecto';
+  if (req.query.respuesta.toLowerCase() === req.quiz.respuesta.toLowerCase()) {
+    resultado = 'Correcto';
+  }
+  res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 };
--- models/models.js
+++ models/models.js
@@ -1,26 +1,48 @@
 var path = require ('path');
 
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
 //Cargar modelo ORM
 var Sequelize = require('sequelize');
 
-//Usar BBDD SQLite
-var sequelize = new Sequelize(null, null, null,
-{dialect: "sqlite", storage:"quiz.sqlite"}
+// Usar BBDD SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd, 
+  { dialect:  protocol,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,  // solo SQLite (.env)
+    omitNull: true      // solo Postgres
+  }      
 );
 
 //Importar la definición de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
+
+
 exports.Quiz = Quiz; //Exportar la definición de la tabla Quiz
 
-//Crear e inicializar la tabla de preguntas en BD
-sequelize.sync().success(function(){
-	Quiz.count().success(function (count){
-		if(count===0){
-			Quiz.create({
-				pregunta: 'Capital de Italia',
-				respuesta: 'Roma'
-			})
-			.success(function(){console.log('Base de datos inicializada')});
-		};
-	});
+// sequelize.sync() inicializa tabla de preguntas en DB
+sequelize.sync().then(function() {
+  // then(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().then(function (count){
+    if(count === 0) {   // la tabla se inicializa solo si está vacía
+      Quiz.bulkCreate( 
+        [ {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
+          {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
+        ]
+      ).then(function(){console.log('Base de datos inicializada')});
+    };
+  });
 });
